9d92cf4202ca2f050c4495fcb43b32dbcffc7049
Irina Skitayeva <irina.skitayeva@gmail.com>
2021-02-07 17:05:01 -0500

    fixed bugs

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const routes = require("./controllers");
+const routes = require("./routes");
 const sequelize = require("./config/connection");
 const path = require("path");
-const helpers = require("./utils/helpers");
+const helpers = require("./routes/utils/helpers");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
 const session = require("express-session");
@@ -34,6 +34,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 });
